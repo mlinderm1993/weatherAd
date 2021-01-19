e8e35286bdd950c582e0d9cc522940da08a7bbaf
marco <marco.lim@live.de>
2021-01-19 21:27:05 +0100

    edited serviceName service

---
+++ backend/services/adService.js
@@ -0,0 +1,94 @@
+const influxClient = require("./services/influxClient");
+const uvClient = require("./services/uvIndexClient");
+
+function AdService() {
+  AdService.prototype.getAd = () => {
+    let latestDataPromise = influxClient.getLatestData();
+    let pressureVariationPromise = influxClient.getVariationInPressure();
+    let temperatureVarationPromise = influxClient.getVariationInTemperature();
+    let uvIndexPromise = uvClient.getUvIndex();
+    let coldTimeRange = [10, 11, 0, 1, 2];
+    let summerTimeRange = [6, 7, 8];
+    let springTimeRange = [0, 1, 2, 3];
+
+    Promise.all([
+      latestDataPromise,
+      pressureVariationPromise,
+      temperatureVarationPromise,
+      uvIndexPromise,
+    ]).then((values) => {
+      let latestData = values[0];
+      let pressureVariation = values[1];
+      let temperatureVaration = values[2];
+      let uvIndexData = values[3];
+      let date = new Date();
+      let month = date.getMonth();
+
+      // Erkältung
+      if (
+        coldTimeRange.indexOf(month) != -1 &&
+        latestData.humidity >= 83 &&
+        latestData.temperature <= 8
+      ) {
+        return {};
+      }
+      // Kreislauf
+      if (
+        isCirculatoryPromlemPossible(latestData, pressureVariation, temperatureVaration, month) &&
+        summerTimeRange.indexOf(month) != -1
+      ) {
+        return {};
+      }
+      // Grippe
+      if (
+        isGrippe(latestData, pressureVariation, temperatureVariation, month) &&
+        springtimeRange.indexOf(month)
+      ) {
+        return {};
+      } // Kopfschmerzen
+      if (headacheTimeRange.indexOf(month) != -1) {
+        return {};
+      }
+    });
+  };
+
+  function isCirculatoryPromlemPossible(latestData, pressureVariation, temperatureVaration, month) {
+    return (
+      (latestData.temperature >= 20 && latestData.humidity >= 80) ||
+      isPressureFluctuating(pressureVariation, month) ||
+      isTemperatureFluctuating(temperatureVaration, month)
+    );
+  }
+
+  function isPressureFluctuating(pressureVariation, month) {
+    if (summerTimeRange.indexOf(month) != -1) {
+      if (pressureVariation >= 15) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function isTemperatureFluctuating(temperatureVaration, month) {
+    if (summerTimeRange.indexOf(month) != -1) {
+      if (temperatureVaration >= 8) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function isGrippe(latestData, pressureVariation, temperatureVariation, month) {
+    if (
+      latestData.temperature <= 5 ||
+      lastData.humidity <= 80 ||
+      isPressureFluctuating(pressureVariation, month) ||
+      isTemperatureFluctuating(temperatureVariation, month)
+    ) {
+      return true;
+    }
+    return false;
+  }
+}
+
+module.exports = new AdService();
